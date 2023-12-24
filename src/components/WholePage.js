b69574c6adb1786be47436f6a624115dd28f17f4
manojconcept <manojveerapavu@gmail.com>
2023-12-24 09:26:24 +0530

    navbar fix

--- src/components/WholePage.js
+++ src/components/WholePage.js
@@ -88,75 +88,78 @@ export function WholePage() {
     console.log(addToCart);
     return (
         <>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <div className="container px-4 px-lg-5">
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
-                            <li className="nav-item"><a className="nav-link active" aria-current="page" href="#!">Home</a></li>
-                            <li className="nav-item"><a className="nav-link" href="#!">About</a></li>
-                            <li className="nav-item dropdown">
-                                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <li><a className="dropdown-item" href="#!">All Products</a></li>
-                                    <li><hr className="dropdown-divider" /></li>
-                                    <li><a className="dropdown-item" href="#!">Popular Items</a></li>
-                                    <li><a className="dropdown-item" href="#!">New Arrivals</a></li>
-                                </ul>
-                            </li>
-                        </ul>
-                        <form className="d-flex">
-                            <button className="btn btn-outline-dark" type="button">
-                                <i className="bi-cart-fill me-1"></i>
-                                Cart
-                                <span className="badge bg-dark text-white ms-1 rounded-pill">{countProductsInCart}</span>
-                            </button>
-                        </form>
+            <div>
+                <nav style={{ position: "sticky", top: "0" ,zIndex: 1000}} className="navbar navbar-expand-lg navbar-light bg-light">
+                    <div className="container px-4 px-lg-5">
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
+                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul className="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
+                                <li className="nav-item"><a className="nav-link active" aria-current="page" href="#!">Home</a></li>
+                                <li className="nav-item"><a className="nav-link" href="#!">About</a></li>
+                                <li className="nav-item dropdown">
+                                    <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                        <li><a className="dropdown-item" href="#!">All Products</a></li>
+                                        <li><hr className="dropdown-divider" /></li>
+                                        <li><a className="dropdown-item" href="#!">Popular Items</a></li>
+                                        <li><a className="dropdown-item" href="#!">New Arrivals</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                            <form className="d-flex">
+                                <button className="btn btn-outline-dark" type="button">
+                                    <i className="bi-cart-fill me-1"></i>
+                                    Cart
+                                    <span className="badge bg-dark text-white ms-1 rounded-pill">{countProductsInCart}</span>
+                                </button>
+                            </form>
+                        </div>
                     </div>
-                </div>
-            </nav>
-            <Header />
-            <section className="py-5">
-                <div className="container px-4 px-lg-5 mt-5">
-                    <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
-                        {
-                            productsList.map((product, index) => {
-                                return (
-                                    <div key={index} className="col mb-5">
-                                        <div className="card h-100">
-                                            <div className="badge bg-dark text-white position-absolute" style={{ top: "0.5rem", right: "0.5rem" }}>Sale</div>
-                                            <img className="card-img-top" style={{ width: "100%", height: "300px" }} src={product.imgAddress} alt="..." />
-                                            <div className="card-body p-4">
-                                                <div className="text-center">
-                                                    <h5 className="fw-bolder">{product.name}</h5>
-                                                    <div className="d-flex justify-content-center small text-warning mb-2">
-                                                        <div className="bi-star-fill"></div>
-                                                        <div className="bi-star-fill"></div>
-                                                        <div className="bi-star-fill"></div>
-                                                        <div className="bi-star-fill"></div>
-                                                        <div className="bi-star-fill"></div>
+                </nav>
+                <Header />
+                <section className="py-5">
+                    <div className="container px-4 px-lg-5 mt-5">
+                        <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+                            {
+                                productsList.map((product, index) => {
+                                    return (
+                                        <div key={index} className="col mb-5">
+                                            <div className="card h-100">
+                                                <div className="badge bg-dark text-white position-absolute" style={{ top: "0.5rem", right: "0.5rem" }}>Sale</div>
+                                                <img className="card-img-top" style={{ width: "100%", height: "300px" }} src={product.imgAddress} alt="..." />
+                                                <div className="card-body p-4">
+                                                    <div className="text-center">
+                                                        <h5 className="fw-bolder">{product.name}</h5>
+                                                        <div className="d-flex justify-content-center small text-warning mb-2">
+                                                            <div className="bi-star-fill"></div>
+                                                            <div className="bi-star-fill"></div>
+                                                            <div className="bi-star-fill"></div>
+                                                            <div className="bi-star-fill"></div>
+                                                            <div className="bi-star-fill"></div>
+                                                        </div>
+                                                        <div>
+                                                            <p>{product.description}</p>
+                                                        </div>
+                                                        <span className="text-muted">${product.price}</span>
                                                     </div>
-                                                    <div>
-                                                        <p>{product.description}</p>
-                                                    </div>
-                                                    <span className="text-muted">${product.price}</span>
+                                                </div>
+                                                <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                                    <div className="text-center"><button onClick={() => { handleToCart(index) }} className="btn btn-outline-dark mt-auto">
+                                                        {
+                                                            addToCart[index].isCart ? "Add to Cart" : "Remove from Cart"
+                                                        }
+                                                    </button></div>
                                                 </div>
                                             </div>
-                                            <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                                                <div className="text-center"><button onClick={() => { handleToCart(index) }} className="btn btn-outline-dark mt-auto">
-                                                    {
-                                                        addToCart[index].isCart ? "Add to Cart" : "Remove from Cart"
-                                                    }
-                                                </button></div>
-                                            </div>
                                         </div>
-                                    </div>
-                                );
-                            })
-                        }
+                                    );
+                                })
+                            }
+                        </div>
                     </div>
-                </div>
-            </section>
-            <Footer />
+                </section>
+                <Footer />
+
+            </div>
         </>
     );
 
